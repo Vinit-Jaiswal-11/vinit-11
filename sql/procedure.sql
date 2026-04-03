e17ee6f28e950c52179a37129b9a6a2c558e6d69
Vinit-Jaiswal-11 <Vinitkumar.jaiswal@operisoft.com>
2026-04-03 17:14:47 +0530

    ci/cd

---
+++ sql/procedure.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE PROCEDURE add_member(
+    p_name VARCHAR,
+    p_age INT,
+    p_email VARCHAR
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO members(name, age, email)
+    VALUES (p_name, p_age, p_email);
+END;
+$$;
